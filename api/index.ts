628fecdc785205a106917e143413465251c2b3f4
Ucupsalah-gntr <calons604@gmail.com>
2026-09-18 03:48:39 +0000

    refactor: remove Manus storage proxy from Vercel API

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { registerStorageProxy } from "../server/_core/storageProxy";
 import { appRouter } from "../server/routers";
 import { createContext } from "../server/_core/context";
 
@@ -9,8 +8,6 @@ const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
-registerStorageProxy(app);
 
 app.use(
   "/api/trpc",
