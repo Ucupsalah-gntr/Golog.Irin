4014eb1cef04adc406fa11c8ff01f8510c10a093
Ucupsalah-gntr <calons604@gmail.com>
2026-09-21 15:39:46 +0700

    fix: use explicit TypeScript imports for Vercel bundling

--- server/usersRouter.ts
+++ server/usersRouter.ts
@@ -1,8 +1,8 @@
 import { TRPCError } from "@trpc/server";
 import { eq } from "drizzle-orm";
 import { z } from "zod";
-import { adminProcedure, router } from "./_core/trpc.js";
-import { getDb, rooms, users, writeAudit } from "./db.js";
+import { adminProcedure, router } from "./_core/trpc.ts";
+import { getDb, rooms, users, writeAudit } from "./db.ts";
 
 export const usersRouter = router({
   list: adminProcedure.query(async () => {
